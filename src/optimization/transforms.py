da1ff085e744114125cd7086ed705d0b5e28e493
piotrhm <helm.piotr@gmail.com>
2021-12-07 10:53:50 +0100

    transforms.py refactored

--- src/optimization/transforms.py
+++ src/optimization/transforms.py
@@ -4,28 +4,41 @@ from typing import Tuple
 
 
 class CustomTransforms:
-    def __init__(
-        self,
-        brightness: float = 1.0,
-        contrast: float = 1.0,
-        saturation: float = 1.0,
-        hue: float = 0.25,
-        rotate: int = 5,
-        scale: Tuple[float, float] = (0.9, 1.1),
-        size: int = 224,
-        kernel_size: int = 7,
-        sigma: float = 2
-    ):
+    """
+    Custom wrapper on torchvision.transforms
+    """
 
+    def __init__(
+            self,
+            color_jitter_args: Tuple[float, float, float, float] = (1.0, 1.0, 1.0, 0.25),
+            random_crop_args: Tuple[int, Tuple[float, float]] = (224, (0.9, 1.1)),
+            rotate: int = 5,
+            gaussian_blur_args: Tuple[int, float] = (7, 2)
+    ):
         """
-        Custom wrapper on torchvision.transforms
+        Parameters:
+            color_jitter_args: Quadruple of (brightness, contrast, saturation, hue)
+            rotate: Max value for range of degrees to select
+            random_crop_args: Tripe of (size, (scale_min, scale_max))
+            gaussian_blur_args: Tuple of (kernel_size, sigma)
         """
+
         self.transforms = tfs.Compose([
-            tfs.ColorJitter(brightness, contrast, saturation, hue),
-            tfs.RandomResizedCrop(size=size, scale=scale),
+            tfs.ColorJitter(color_jitter_args[0], color_jitter_args[1], color_jitter_args[2], color_jitter_args[3]),
+            tfs.RandomResizedCrop(size=random_crop_args[0], scale=random_crop_args[1]),
             tfs.RandomRotation(range(-rotate, rotate + 1)),
-            tfs.GaussianBlur(kernel_size=kernel_size, sigma=sigma)
+            tfs.GaussianBlur(kernel_size=gaussian_blur_args[0], sigma=gaussian_blur_args[1])
         ])
 
     def __call__(self, x):
         return self.transforms(x)
+
+# Predefined transforms
+
+
+transforms_base = tfs.Compose([
+    tfs.ColorJitter(brightness=1.0, contrast=1.0, saturation=1.0, hue=0.25),
+    tfs.RandomResizedCrop(size=224, scale=(0.9, 1.1)),
+    tfs.RandomRotation(range(-5, 6)),
+    tfs.GaussianBlur(kernel_size=7, sigma=2)
+])
